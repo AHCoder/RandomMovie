8b7ccb2645c7ffe9f74e4e75621b6b7ff2178e40
AHCoder <AHCoder@users.noreply.github.com>
2016-07-28 23:29:09 +0200

    Add project files.

---
+++ RandomMovie/src/view/MainWindow.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace RandomMovie.src.view
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(355, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(367, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(371, 173);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "GO!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(89, 175);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(48, 19);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "VLC";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(484, 169);
+            this.tabControl1.TabIndex = 9;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox6);
+            this.tabPage1.Controls.Add(this.textBox5);
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.button7);
+            this.tabPage1.Controls.Add(this.button6);
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(476, 141);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Choose Folders";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(6, 109);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(355, 22);
+            this.textBox6.TabIndex = 16;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(6, 80);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(355, 22);
+            this.textBox5.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(6, 49);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(355, 22);
+            this.textBox4.TabIndex = 14;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(367, 108);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(87, 23);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "Browse...";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(367, 79);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(87, 23);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "Browse...";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(367, 48);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(87, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Browse...";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(476, 141);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Player Paths";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(365, 48);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "VLC...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(365, 18);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "MPC-HC...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 48);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(353, 22);
+            this.textBox3.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 18);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(353, 22);
+            this.textBox2.TabIndex = 0;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(271, 176);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(81, 19);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "Fullscreen";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(154, 176);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(111, 19);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Jump 20 Min. In";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(10, 175);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(73, 19);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "MPC-HC";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(484, 207);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button2);
+            this.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Random Movie";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+
+    }
+}
---
+++ RandomMovie/src/view/MainWindow.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using RandomMovie.Properties;
+
+namespace RandomMovie.src.view
+{
+    public partial class MainWindow : Form
+    {
+
+        private FolderBrowserDialog fbd;
+        private OpenFileDialog ofd;
+
+        // File types to look for, folders to exclude
+        private String[] extensions = new String[] { ".mkv", ".mp4", ".m4p", ".m4v", ".vob", ".avi", ".mov", ".wmv", ".mpg", ".mpeg", ".m2v" },
+                         excFolder = new String[] { "behind the scenes", "deleted scenes", "interviews", "trailers" };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // First Browse Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = fbd.SelectedPath;
+            }
+        }
+
+        // GO! Button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String fileName, arguments;
+
+            if (radioButton1.Checked)
+            {
+                fileName = textBox2.Text;
+                arguments = "\"" + chooseFile();
+                if (checkBox1.Checked)
+                {
+                    arguments += "\" /startpos 00:20:00";
+                }
+                if (checkBox2.Checked)
+                {
+                    arguments += " /fullscreen";
+                }
+            }
+            else
+            {
+                fileName = textBox3.Text;
+                arguments = "\"" + chooseFile();
+                if (checkBox1.Checked)
+                {
+                    arguments += "\" --start-time=1200";
+                }
+                if (checkBox2.Checked)
+                {
+                    arguments += " -f";
+                }
+            }
+            Process.Start(fileName, arguments);
+
+        }
+
+        // MPC-HC Path Button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox2.Text = ofd.FileName;
+            }
+        }
+
+        // VLC Path Button
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox3.Text = ofd.FileName;
+            }
+        }
+
+        // Second
+        private void button5_Click(object sender, EventArgs e)
+        {
+            fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox4.Text = fbd.SelectedPath;
+            }
+        }
+
+        // Third
+        private void button6_Click(object sender, EventArgs e)
+        {
+            fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox5.Text = fbd.SelectedPath;
+            }
+        }
+
+        // Fourth
+        private void button7_Click(object sender, EventArgs e)
+        {
+            fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox6.Text = fbd.SelectedPath;
+            }
+        }
+
+        // Method for choosing the random files
+        private String chooseFile()
+        {
+            var rand = new Random();
+
+            // Get the files from each directory
+            var files = Directory.EnumerateFiles(textBox1.Text, ".", SearchOption.AllDirectories).Where(f => extensions.Contains(Path.GetExtension(f).ToLower()) &&
+                                                                                                             !excFolder.Any(d => Path.GetDirectoryName(f).ToLower().Contains(d))).ToList();
+            if(textBox4.Text != "")
+                files.AddRange(Directory.EnumerateFiles(textBox4.Text, ".", SearchOption.AllDirectories).Where(f => extensions.Contains(Path.GetExtension(f).ToLower()) &&
+                                                                                                                    !excFolder.Any(d => Path.GetDirectoryName(f).ToLower().Contains(d))));
+            if(textBox5.Text != "")
+                files.AddRange(Directory.EnumerateFiles(textBox5.Text, ".", SearchOption.AllDirectories).Where(f => extensions.Contains(Path.GetExtension(f).ToLower()) &&
+                                                                                                                    !excFolder.Any(d => Path.GetDirectoryName(f).ToLower().Contains(d))));
+            if(textBox6.Text != "")
+                files.AddRange(Directory.EnumerateFiles(textBox6.Text, ".", SearchOption.AllDirectories).Where(f => extensions.Contains(Path.GetExtension(f).ToLower()) &&
+                                                                                                                    !excFolder.Any(d => Path.GetDirectoryName(f).ToLower().Contains(d))));
+            // Choose a file at random
+            return files.ElementAt(rand.Next(0, files.Count()));
+        }
+
+        // Settings on Close and Load
+        private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Settings.Default.browse1 = textBox1.Text;
+            Settings.Default.browse2 = textBox4.Text;
+            Settings.Default.browse3 = textBox5.Text;
+            Settings.Default.browse4 = textBox6.Text;
+            Settings.Default.mpcText = textBox2.Text;
+            Settings.Default.vlcText = textBox3.Text;
+            if (radioButton1.Checked)
+            {
+                Settings.Default.defPlay = "mpc";
+            }
+            else
+            {
+                Settings.Default.defPlay = "vlc";
+            }
+            if (checkBox1.Checked)
+            {
+                Settings.Default.checkStatus1 = true;
+            }
+            else
+            {
+                Settings.Default.checkStatus1 = false;
+            }
+            if (checkBox2.Checked)
+            {
+                Settings.Default.checkStatus2 = true;
+            }
+            else
+            {
+                Settings.Default.checkStatus2 = false;
+            }
+            Settings.Default.Save();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = Settings.Default.browse1;
+            textBox4.Text = Settings.Default.browse2;
+            textBox5.Text = Settings.Default.browse3;
+            textBox6.Text = Settings.Default.browse4;
+            textBox2.Text = Settings.Default.mpcText;
+            textBox3.Text = Settings.Default.vlcText;
+            if (Settings.Default.defPlay == "mpc")
+            {
+                radioButton1.Checked = true;
+            }
+            else
+            {
+                radioButton2.Checked = true;
+            }
+            checkBox1.Checked = Settings.Default.checkStatus1;
+            checkBox2.Checked = Settings.Default.checkStatus2;
+        }
+    }
+}
